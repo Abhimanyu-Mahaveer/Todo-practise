e004bf359c7844572e1ff0d8c0089082db5fcea4
Abhimanyu-Mahaveer <karmakarsuprotim29@gmail.com>
2024-02-20 02:46:58 +0530

    Some verification of zod added on index.js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,23 @@
 const express =require('express') 
+const { createTodo, updateTodo } = require('./type')
 const app=express() 
 const port=3000
 app.use(express.json())
 
 app.post('/todo', function(req,res)
 {
+    const createPayLoad=req.body; //the user input that is requested from browser 
+    const parsePayLoad=createTodo.safeParse(createPayLoad); //parsing the body using zod for validation
+    //message if the inputs are given wrong
+    if(!parsePayLoad.success){ 
+        res.status(411).json({
+            msg:"Your input is wrong"
+        }); 
+    return; 
+    }
+    //mongo db logic -> 
 
-
+    
 })
 
 app.get('/todo', function(req, res){
@@ -14,6 +25,13 @@ app.get('/todo', function(req, res){
 })
 
 app.put('/completed', function(req, res){
-     
+    const createPayLoad=req.body; 
+    const parsePayLoad=updateTodo.safeParse(createPayLoad); 
+    if(!parsePayLoad.success){
+        res.status(411).json({
+            msg:"Your input is wrong"
+        })
+    return;
+    }
 })
 app.listen(port);  
